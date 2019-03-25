ad869adec46be437534d7246903e22278855f830
jz <zhouj@iresdev1.(none)>
2019-03-25 11:46:25 +1100

    migrate to jdk11

---
+++ src/main/java/com/jz/nebula/controller/PaymentController.java
@@ -0,0 +1,29 @@
+package com.jz.nebula.controller;
+
+import javax.annotation.security.RolesAllowed;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+//import com.jz.nebula.entity.Payment;
+import com.jz.nebula.entity.Role;
+//import com.jz.nebula.service.PaymentService;
+//import com.stripe.model.Charge;
+
+@RestController
+@RequestMapping("/payments")
+public class PaymentController {
+//	@Autowired
+//	private PaymentService paymentService;
+	
+	@PostMapping("")
+	@RolesAllowed({Role.ROLE_USER, Role.ROLE_VENDOR, Role.ROLE_ADMIN})
+	public @ResponseBody Object create(@RequestBody Object payment) throws Exception {
+		return "test";
+	}
+    
+}
