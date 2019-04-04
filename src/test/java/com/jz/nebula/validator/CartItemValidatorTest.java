b32891430728707ac51193538ba1ed2072e67c58
Joe <ashzhouyue@gmail.com>
2019-04-04 16:23:57 +1100

    add tests

--- src/test/java/com/jz/nebula/validator/CartItemValidatorTest.java
+++ src/test/java/com/jz/nebula/validator/CartItemValidatorTest.java
@@ -1,10 +1,15 @@
 package com.jz.nebula.validator;
 
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.jz.nebula.util.Security;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
@@ -14,5 +19,8 @@ public class CartItemValidatorTest {
 	@Autowired
 	CartItemValidator cartItemValidator;
 	
-	
+	@Test
+	public void getRandomHashTest() {
+		assertTrue(true);
+	}
 }
