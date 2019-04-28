c8688e5012c359d9897cece89e6d6d862c50ed4e
Joe <ashzhouyue@gmail.com>
2019-04-28 12:27:33 +1000

    convert to war

--- src/main/java/com/jz/nebula/Application.java
+++ src/main/java/com/jz/nebula/Application.java
@@ -4,13 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import com.jz.nebula.payment.StripeProperties;
 
+// Remove SpringBootServletInitializer when change to jar build
 @EnableAutoConfiguration
 @SpringBootApplication
 @EnableConfigurationProperties(StripeProperties.class)
-public class Application {
+public class Application extends SpringBootServletInitializer{
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
