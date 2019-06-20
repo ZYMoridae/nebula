c9e7388ab8386dcb72aaf6cc9289425808816321
Joe <ashzhouyue@gmail.com>
2019-06-20 23:28:30 +1000

    tweak

--- src/main/java/com/jz/nebula/controller/UserController.java
+++ src/main/java/com/jz/nebula/controller/UserController.java
@@ -1,25 +1,17 @@
 package com.jz.nebula.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jz.nebula.auth.IAuthenticationFacade;
-import com.jz.nebula.dao.RoleRepository;
 import com.jz.nebula.dao.UserRepository;
-import com.jz.nebula.dao.UserRolesRepository;
-import com.jz.nebula.entity.Role;
 import com.jz.nebula.entity.User;
-import com.jz.nebula.entity.UserRole;
-import com.jz.nebula.service.TokenService;
+import com.jz.nebula.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.*;
+import java.util.Map;
 
-import static java.util.stream.Collectors.toList;
 import static org.springframework.http.ResponseEntity.ok;
 
 @RestController
@@ -29,32 +21,18 @@ public class UserController {
     UserRepository userRepository;
 
     @Autowired
-    RoleRepository roleRepository;
-
-    @Autowired
-    TokenService jwtTokenProvider;
-
-    @Autowired
-    UserRolesRepository userRolesRepository;
-
-    @Autowired
     private IAuthenticationFacade authenticationFacade;
 
+    @Autowired
+    private UserService userService;
+
     /**
-     *
      * @param userDetails
      * @return
      */
     @GetMapping("/me")
     public ResponseEntity<?> currentUser(@AuthenticationPrincipal UserDetails userDetails) {
-        Map<Object, Object> model = new HashMap<>();
-        System.out.println(userDetails.getUsername());
-        model.put("username", userDetails.getUsername());
-        model.put("roles", userDetails.getAuthorities().stream().map(a -> ((GrantedAuthority) a).getAuthority())
-                .collect(toList()));
-
-        User user = userRepository.findByUsername(userDetails.getUsername()).get();
-
+        User user = userService.getCurrentUser(userDetails);
         return ok(user);
     }
 
@@ -67,39 +45,11 @@ public class UserController {
     @PostMapping("")
     public ResponseEntity<?> createUser(@RequestBody User user) {
         // TODO: Move logic into user service
-        user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
-        ArrayList<Role> rolesList = new ArrayList<>();
-        Role normalUser = roleRepository.findByCode(Role.ROLE_USER.replaceAll("ROLE_", "")).get();
-
-        rolesList.add(normalUser);
-        user.setRoles(rolesList);
-        ObjectMapper oMapper = new ObjectMapper();
-        User savedUser = userRepository.save(user);
-
-        UserRole userRole = new UserRole();
-        userRole.setRoleId(normalUser.getId());
-        userRole.setUserId(savedUser.getId());
-        userRole = userRolesRepository.save(userRole);
-
-//        Set<UserRole> userRoleList = new HashSet<>();
-//        userRoleList.add(userRole);
-//        user.setUserRoles(userRoleList);
-
-        savedUser = userRepository.findById(savedUser.getId()).get();
-
-        @SuppressWarnings("unchecked")
-        Map<String, Object> map = oMapper.convertValue(savedUser, Map.class);
-        List<String> roles = new ArrayList<>();
-        roles.add(Role.ROLE_USER);
-        Map<String, String> tokenMap = jwtTokenProvider.createToken(savedUser.getUsername(), rolesList);
-        map.put("token", tokenMap.get("accessToken"));
-        map.put("refreshToken", tokenMap.get("refreshToken"));
-
-        return ok(map);
+        Map userResultMap = userService.createUser(user);
+        return ok(userResultMap);
     }
 
     /**
-     *
      * @param id
      * @param user
      * @return
--- src/main/java/com/jz/nebula/service/UserService.java
+++ src/main/java/com/jz/nebula/service/UserService.java
@@ -1,5 +1,14 @@
 package com.jz.nebula.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jz.nebula.dao.RoleRepository;
+import com.jz.nebula.dao.UserRepository;
+import com.jz.nebula.dao.UserRolesRepository;
+import com.jz.nebula.entity.Role;
+import com.jz.nebula.entity.User;
+import com.jz.nebula.entity.UserRole;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User.UserBuilder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -8,19 +17,29 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.jz.nebula.dao.UserRepository;
-import com.jz.nebula.entity.User;
-
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
 
 @Service("userDetailsService")
 public class UserService implements UserDetailsService {
+    private final Logger logger = LogManager.getLogger(UserService.class);
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private UserRolesRepository userRolesRepository;
+
+    @Autowired
+    private TokenService jwtTokenProvider;
+
+    @Autowired
+    private RoleRepository roleRepository;
 
     /**
      * Load user from database by username
@@ -39,5 +58,56 @@ public class UserService implements UserDetailsService {
         builder.roles(roleArray);
 
         return builder.build();
+    }
+
+    /**
+     * Create user
+     *
+     * @param user
+     * @return
+     */
+    public Map createUser(User user) {
+        user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
+        ArrayList<Role> rolesList = new ArrayList<>();
+        Role normalUser = roleRepository.findByCode(Role.ROLE_USER.replaceAll("ROLE_", "")).get();
+
+        rolesList.add(normalUser);
+        user.setRoles(rolesList);
+        ObjectMapper oMapper = new ObjectMapper();
+        User savedUser = userRepository.save(user);
+
+        UserRole userRole = new UserRole();
+        userRole.setRoleId(normalUser.getId());
+        userRole.setUserId(savedUser.getId());
+        userRolesRepository.save(userRole);
+
+        // Find user by id again to get user roles
+        savedUser = userRepository.findById(savedUser.getId()).get();
+
+        Map<String, Object> map = oMapper.convertValue(savedUser, Map.class);
+        List<String> roles = new ArrayList<>();
+        roles.add(Role.ROLE_USER);
+        Map<String, String> tokenMap = jwtTokenProvider.createToken(savedUser.getUsername(), rolesList);
+        map.put("token", tokenMap.get("accessToken"));
+        map.put("refreshToken", tokenMap.get("refreshToken"));
+
+        return tokenMap;
+    }
+
+    /**
+     * Get current user through user details
+     *
+     * @param userDetails
+     * @return
+     */
+    public User getCurrentUser(UserDetails userDetails) {
+        Map<Object, Object> model = new HashMap<>();
+        model.put("username", userDetails.getUsername());
+        model.put("roles", userDetails.getAuthorities().stream().map(a -> a.getAuthority())
+            .collect(toList()));
+
+        User user = userRepository.findByUsername(userDetails.getUsername()).get();
+
+        return user;
     }
 }
