8242079b5692f3985f6f0ba9d30ae6827c604e39
Joe <ashzhouyue@gmail.com>
2019-06-27 23:27:30 +1000

    add token live check api

--- src/main/java/com/jz/nebula/controller/TokenController.java
+++ src/main/java/com/jz/nebula/controller/TokenController.java
@@ -2,6 +2,7 @@ package com.jz.nebula.controller;
 
 import com.jz.nebula.entity.Role;
 import com.jz.nebula.service.RefreshTokenService;
+import com.jz.nebula.service.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,6 +15,9 @@ import java.util.concurrent.ConcurrentHashMap;
 public class TokenController {
     @Autowired
     RefreshTokenService refreshTokenService;
+
+    @Autowired
+    TokenService tokenService;
 
     /**
      *
@@ -38,6 +42,22 @@ public class TokenController {
         Map<String, String> resultMap = new ConcurrentHashMap<>();
         String refreshToken = refreshTokenService.getRefreshToken();
         resultMap.put("refreshToken", refreshToken);
+        return resultMap;
+    }
+
+    /**
+     * Check token is alive or not
+     *
+     * @param token
+     * @return
+     */
+    @GetMapping("/{token}")
+    @RolesAllowed({Role.ROLE_USER, Role.ROLE_VENDOR, Role.ROLE_ADMIN})
+    public @ResponseBody
+    Map<String, Boolean> isTokenAlive(@PathVariable("token") String token) {
+        Map<String, Boolean> resultMap = new ConcurrentHashMap<>();
+        Boolean isTokenAlive = tokenService.validateToken(token);
+        resultMap.put("isTokenAlive", isTokenAlive);
         return resultMap;
     }
 }
